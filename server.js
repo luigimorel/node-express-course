da0dd814f65c3472c1249c79da3c5280f098b210
morelmiles <morelmiles@gmail.com>
2020-05-04 23:55:31 +0300

    Added the mock data

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const mockUserData = [{ name: "Luigi" }, { name: "Morel" }];
+
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got the users!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, function () {
   console.log("server is running");
 });
